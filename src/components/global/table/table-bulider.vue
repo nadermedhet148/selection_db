7deb248bb02e32d62dbf05b699169e81c57302db
nadermedhet148 <nadermedhet142@gmail.com>
2022-01-21 15:18:13 +0200

    [feat] add title to table componet

--- src/components/global/table/table-bulider.vue
+++ src/components/global/table/table-bulider.vue
@@ -1,79 +1,62 @@
 <template>
-  <div>
-        <v-card class="mt-8">
-      <v-card-title>
-        المؤثرات
-        <v-spacer></v-spacer>
-        <printer-menu
-          :disabled="items.length == 0"
-          :data="printer"
-          :fab="false"
-        ></printer-menu>
-      </v-card-title>
-      <v-divider></v-divider>
-      <v-data-table
-        :headers="headers.filter(h => h.inTable)"
-        :items="items"
-        hide-default-header
-        fixed-header
-        multi-sort
-        ref="mainTable"
+<div>
+    <v-card class="mt-8">
+        <v-card-title>
+            {{title}}
+            <v-spacer></v-spacer>
+            <printer-menu :disabled="items.length == 0" :data="printer" :fab="false"></printer-menu>
+        </v-card-title>
+        <v-divider></v-divider>
+        <v-data-table :headers="headers.filter(h => h.inTable)" :items="items" hide-default-header fixed-header multi-sort ref="mainTable">
 
-      >
+            <template v-slot:header="table">
+                <table-header-filters :items="items" :table="table" :filters.sync="tableFilters"></table-header-filters>
+            </template>
+            <template v-slot:footer="table">
+                <table-footer-filters :filters.sync="tableFilters" :table="table"></table-footer-filters>
+            </template>
 
-       <template v-slot:header="table">
-          <table-header-filters
-            :items="items"
-            :table="table"
-            :filters.sync="tableFilters"
-          ></table-header-filters>
-        </template>
-        <template v-slot:footer="table">
-          <table-footer-filters
-            :filters.sync="tableFilters"
-            :table="table"
-          ></table-footer-filters>
-        </template>
+            <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope">
+                <slot :name="slot" v-bind="scope" /></template>
 
-
-
-        <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope"><slot :name="slot" v-bind="scope"/></template>
-
-
-      </v-data-table>
+        </v-data-table>
     </v-card>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "table-bulider",
-   filters: {
-    filterStrLimit(str, limit) {
-      return `${str.length <= limit ? str : str.substr(0, limit) + ".."}`;
-    }
-  },
-  props: {
-    headers: {
-      type: Array,
-      default: () => ([])
+    name: "table-bulider",
+    filters: {
+        filterStrLimit(str, limit) {
+            return `${str.length <= limit ? str : str.substr(0, limit) + ".."}`;
+        }
     },
+    props: {
+        headers: {
+            type: Array,
+            default: () => ([])
+        },
         items: {
-      type: Array,
-      default: () => ([])
+            type: Array,
+            default: () => ([])
+        },
+        printer: {
+            type: Object,
+            default: () => ({})
+        },
+        title: {
+           type: String,
+            default: () => ""
+        }
     },
-    printer: {
-      type: Object,
-      default: () => ({})
+    data: () => ({
+        tableFilters: {},
+    }),
+    methods: {
+        removeFilter(key) {
+            this.$set(this.filters, key, undefined);
+        }
     }
-  },
-  data: () => ({
-    tableFilters: {},
-  }),
-  methods: {
-    removeFilter(key) {
-      this.$set(this.filters, key, undefined);
-    }
-  }
 };
 </script>
